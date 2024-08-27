93c09c62cd119304da8eb6e595360086524ce5ac
Dave Schick <schick@mtco.com>
2024-08-27 06:19:17 -0500

    java 9 examples

--- src/funwithjava8defaultmethods/Genesis.java
+++
@@ -1,8 +0,0 @@
-package funwithjava8defaultmethods;
-
-public class Genesis implements Car {
-    @Override
-    public void setUpAWD(String awdDetails) {
-        System.out.println("AWD input: " + awdDetails);
-    }
-}
